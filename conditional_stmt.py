8c25324b6137861c6d5edca68962f3c03924aea2
aatmanrajyaguru <aatmanrajyaguru@gmail.com>
2026-03-25 11:15:54 +0530

    Worked on the coditipna; statements

---
+++ conditional_stmt.py
@@ -0,0 +1,12 @@
+# Program to demonstrate if-elif statements with user input
+
+age = int(input("Enter your age: "))
+
+if age < 13:
+    print("You are a child.")
+elif age < 18:
+    print("You are a teenager.")
+elif age < 60:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
