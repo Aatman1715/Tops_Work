9a07aac6a87179f88493c55897faeaa82e588341
Aatman Rajyaguru <aatmanrajyaguru@gmail.com>
2026-04-13 18:27:51 +0530

    Forl loop file commit

--- forloop.py
+++ forloop.py
@@ -1,12 +1,12 @@
 #Simple * pyramid pattern using for loop
-# rows = int(input("Enter the number of rows: "))
-# for i in range(0, rows):  
-#     #Inner for loop to handle number of columns  
-#     #values change according to the outer loop  
-#         for j in range(0, i + 1):  
-#             print("* ", end=" ")       
-#         #End line after each row  
-#         print()
+rows = int(input("Enter the number of rows: "))
+for i in range(0, rows):  
+    #Inner for loop to handle number of columns  
+    #values change according to the outer loop  
+        for j in range(0, i + 1):  
+            print("* ", end=" ")       
+        #End line after each row  
+        print()
 
 #Simple alphabet pyramid pattern using for loop
 # rows = int(input("Enter the number of rows: "))  
